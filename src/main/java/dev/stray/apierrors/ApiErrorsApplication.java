079ad5394b8b678f9f476f577abecdaba4be7fa3
noradroid <75635291+noradroid@users.noreply.github.com>
2023-09-21 21:40:30 +0800

    Initial commit

---
+++ src/main/java/dev/stray/apierrors/ApiErrorsApplication.java
@@ -0,0 +1,13 @@
+package dev.stray.apierrors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiErrorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiErrorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/stray/apierrors/ApiErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stray.apierrors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiErrorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
